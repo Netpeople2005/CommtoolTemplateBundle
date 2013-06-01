0c05301e43b4ab764bec0c75bd7cfd552ebd1c16
manuel_j555 <programador.manuel@gmail.com>
2013-06-01 14:21:27 -0430

    primer commit

---
+++ CommtoolTemplateBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Optime\Commtool\TemplateBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CommtoolTemplateBundle extends Bundle
+{
+}
---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Optime\Commtool\TemplateBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('CommtoolTemplateBundle:Default:index.html.twig', array('name' => ''));
+    }
+}
